37421ed28a81b8c0fc327045fe44d1087bd423d0
Jamshid Jumaboyev <jamshidjumaboyev2204@mail.com>
2023-01-19 22:09:19 +0500

    first commit

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Button from "../Button/Button";
 import './ProductItem.css';
+import { useState } from 'react';
 
 const ProductItem = ({product, className, onAdd}) => {
+    // const [color, setColor] = useState("blue");
+    const [text, setText] = useState("Qoshish");
 
     const onAddHandler = () => {
+        if (text === "Qoshish") {
+            setText("Qoshildi");
+            onAdd(product);
+            // setColor("red");
+        } else {
+            setText("Qoshish");
+            onAdd(product);
+            // setColor("red");
+        }
         onAdd(product);
     }
 
@@ -16,7 +28,7 @@ const ProductItem = ({product, className, onAdd}) => {
                 <span>Summasi: <b>{product.price}</b></span>
             </div>
             <Button className={'add-btn'} onClick={onAddHandler}>
-                Qo'shish
+                {text}
             </Button>
         </div>
     );
